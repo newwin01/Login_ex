733f2319470d62868f40b4beb01b44a0b585093d
newwin01 <newwin0198@handong.ac.kr>
2022-12-06 23:46:20 +0900

    First Commit

---
+++ src/main/java/com/example/board/HomeController.java
@@ -0,0 +1,20 @@
+package com.example.board;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class HomeController {
+    @Autowired
+    BoardService boardService;
+
+    @RequestMapping("/")
+    public String Home(){
+        return "login";
+    }
+
+}
